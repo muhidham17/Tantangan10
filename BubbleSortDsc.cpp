0fbc5359cf19f24b76676babe0c0609e1db947e6
muhidham17 <49118793+muhidham17@users.noreply.github.com>
2019-04-08 22:19:11 +0800

    Mengupdate listing agar terurut dari > ke <
    Menghapus "int xpp = *yp;" pada baris ke 6, Mengubah "for(i=0;i<n-1;i++)" menjadi "for(i=n-1;i>=0;i--) pada baris ke 12, Mengubah "for(j=0; j<n-i-1; j++)" Menjadi for(j=0;j<=n-1;j++)" pada baris ke 13, dan Mengubah "if(arr[j]>arr[j+1])" menjadi "if(arr[j]<arr[j+1])" pada baris ke 14

--- BubbleSortDsc.cpp
+++ BubbleSortDsc.cpp
@@ -3,15 +3,14 @@ void swap(int *xp, int *yp)
 {
   int temp = *xp;
   *xp = *yp;
-  int xpp = *yp;
   *yp = temp;
 }
 void bubblesort(int arr[], int n)
 {
   int i,j;
-  for (i=0;i<n-1;i++)
-    for (j=0;j<n-i-1;j++)
-      if (arr[j]>arr[j+1])
+  for (i=n-1;i>=0;i--)
+    for (j=0;j<=n-1;j++)
+      if (arr[j]<arr[j+1])
         swap(&arr[j], &arr[j+1]);
 }
 void printarray (int arr[], int size)
